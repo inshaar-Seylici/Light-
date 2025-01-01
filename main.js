d99eda36e9c61d624b06ae220f59fc9c0c2014dd
inshaar-157 <seylici157@gmail.com>
2025-01-01 06:26:57 +0300

    light using Tailwind and JavaScript

---
+++ main.js
@@ -0,0 +1,15 @@
+let box = document.querySelector('#light')
+
+let button1 = document.querySelector('#btnOn')
+let button2 = document.querySelector('#btnOff')
+
+button1.addEventListener('click', function(){
+
+    box.style.backgroundColor = 'red'
+})
+
+button2.addEventListener('click', function(){
+    box.style.backgroundColor = 'transparent'
+})
+
+
